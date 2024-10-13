d172de9c2532e698659225f72fb90ce2002dd8d9
juanchigit <juanituite@gmail.com>
2024-10-13 19:20:00 -0300

    header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { OrdenTrabajoComponent } from './orden-trabajo/orden-trabajo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent 
+    LoginComponent,
+    HeaderComponent,
+    OrdenTrabajoComponent 
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/orden-trabajo/enums.ts
@@ -0,0 +1,40 @@
+export enum Edificio {
+    A = 'Edificio A',
+    B = 'Edificio B',
+    C = 'Edificio C'
+  }
+  
+  export enum Activo {
+    SI = 'Sí',
+    NO = 'No'
+  }
+  
+  export enum Sector {
+    IT = 'IT',
+    HR = 'Recursos Humanos',
+    MKT = 'Marketing'
+  }
+  
+  export enum Piso {
+    PRIMER = 'Primer Piso',
+    SEGUNDO = 'Segundo Piso'
+  }
+  
+  export enum Ubicacion {
+    SALA1 = 'Sala 1',
+    SALA2 = 'Sala 2'
+  }
+  
+  export enum Operario {
+    JUAN = 'Juan Pérez',
+    MARIA = 'María López'
+  }
+  
+  export enum Tarea {
+    Limpiar = "Limpiar",
+    Organizar = "Organizar",
+    Revisar = "Revisar",
+    Reparar = "Reparar",
+    Otros = "Otros"
+}
+
