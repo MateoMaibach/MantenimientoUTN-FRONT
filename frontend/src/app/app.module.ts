69b32506186f30e33c4a206f46dc84eca07b868e
juanmartinh13 <juanmartinherrera13@gmail.com>
2024-10-13 19:46:18 -0300

    registro

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { HeaderadmComponent } from './components/headeradm/headeradm.component';
+import { OperariosComponent } from './components/operarios/operarios.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent 
+    LoginComponent,
+    HeaderadmComponent,
+    OperariosComponent,
+    RegistroComponent 
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/headeradm/headeradm.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headeradm',
+  templateUrl: './headeradm.component.html',
+  styleUrl: './headeradm.component.css'
+})
+export class HeaderadmComponent {
+
+}
