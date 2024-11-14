83a9fce1033c17f31db216e250ad39d2a4081358
IvanSekul147 <137842170+IvanSekul147@users.noreply.github.com>
2024-11-14 19:55:02 -0300

    modificacion gestor

--- frontend/src/app/services/sector.service.ts
+++ frontend/src/app/services/sector.service.ts
@@ -7,22 +7,22 @@ import { Observable } from 'rxjs';
 })
 export class SectorService {
 
-  private apiUrl = 'http://localhost:3000/api/sector'; // Cambia la URL si es necesario
+  private apiUrl = 'http://localhost:3000/api/sector';
 
   constructor(private http: HttpClient) { }
 
-  // Obtener la lista de sectores
+  
   getSector(): Observable<any> {
     return this.http.get(`${this.apiUrl}`);
   }
 
-  // Agregar un nuevo sector
+  
   addSector(sector: any): Observable<any> {
     return this.http.post(`${this.apiUrl}`, sector);
   }
 
-  // Eliminar un sector por ID o nombre, según lo que maneje la API
-  deleteSector(id: any): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${id}`);
+  
+  deleteSector(nombre: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}?nombre=${nombre}`);
   }
 }
